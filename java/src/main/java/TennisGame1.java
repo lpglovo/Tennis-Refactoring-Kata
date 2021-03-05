cf825f2318335db9265f2f15dc65100ce0ee3a38
lucapandini <gVWvs$hIKNU3yc$fmHOj>
2021-03-05 15:37:58 +0100

    use player names instead of literal strings

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int scorePlayer1 = 0;
     private int scorePlayer2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,7 +12,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(player1Name))
             scorePlayer1 += 1;
         else
             scorePlayer2 += 1;
@@ -43,10 +43,10 @@ public class TennisGame1 implements TennisGame {
         else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
             int minusResult = scorePlayer1 - scorePlayer2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) score ="Advantage " + player1Name;
+            else if (minusResult ==-1) score ="Advantage " + player2Name;
+            else if (minusResult>=2) score = "Win for " + player1Name;
+            else score ="Win for " + player2Name;
         }
         else
         {
