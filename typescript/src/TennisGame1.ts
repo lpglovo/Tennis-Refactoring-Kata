bfeaf6d2fbd809476a51e246ba15f308a0de6dc0
Damian Osipiuk <osipiukd+git@gmail.com>
2021-03-05 15:40:28 +0100

    extract variables to objects

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,67 +1,68 @@
-import { TennisGame } from './TennisGame';
-
+import { TennisGame } from "./TennisGame";
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
+  private player1 = {
+    score: 0,
+    name: "",
+  };
+  private player2 = {
+    score: 0,
+    name: "",
+  };
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+    this.player1.name = player1Name;
+    this.player2.name = player2Name;
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
+    if (playerName === this.player1.name) this.player1.score += 1;
+    else this.player2.score += 1;
   }
 
   getScore(): string {
-    let score: string = '';
+    let score: string = "";
     let tempScore: number = 0;
-    if (this.m_score1 === this.m_score2) {
-      switch (this.m_score1) {
+    if (this.player1.score === this.player2.score) {
+      switch (this.player1.score) {
         case 0:
-          score = 'Love-All';
+          score = "Love-All";
           break;
         case 1:
-          score = 'Fifteen-All';
+          score = "Fifteen-All";
           break;
         case 2:
-          score = 'Thirty-All';
+          score = "Thirty-All";
           break;
         default:
-          score = 'Deuce';
+          score = "Deuce";
           break;
-
       }
-    }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    }
-    else {
+    } else if (this.player1.score >= 4 || this.player2.score >= 4) {
+      const minusResult: number = this.player1.score - this.player2.score;
+      if (minusResult === 1) score = "Advantage player1";
+      else if (minusResult === -1) score = "Advantage player2";
+      else if (minusResult >= 2) score = "Win for player1";
+      else score = "Win for player2";
+    } else {
       for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
+        if (i === 1) tempScore = this.player1.score;
+        else {
+          score += "-";
+          tempScore = this.player2.score;
+        }
         switch (tempScore) {
           case 0:
-            score += 'Love';
+            score += "Love";
             break;
           case 1:
-            score += 'Fifteen';
+            score += "Fifteen";
             break;
           case 2:
-            score += 'Thirty';
+            score += "Thirty";
             break;
           case 3:
-            score += 'Forty';
+            score += "Forty";
             break;
         }
       }
