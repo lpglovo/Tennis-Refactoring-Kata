46d5c873cf449b2e348ec96f11a76fdd2750858f
lucapandini <gVWvs$hIKNU3yc$fmHOj>
2021-03-05 15:49:00 +0100

    give a name to the score using methods

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -21,56 +21,80 @@ public class TennisGame1 implements TennisGame {
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (scorePlayer1 == scorePlayer2)
+        if (isScoreEven())
         {
-            switch (scorePlayer1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = scoreWhenPlayersAreEven();
         }
         else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = scorePlayer1 - scorePlayer2;
-            if (minusResult==1) score ="Advantage " + player1Name;
-            else if (minusResult ==-1) score ="Advantage " + player2Name;
-            else if (minusResult>=2) score = "Win for " + player1Name;
-            else score ="Win for " + player2Name;
+            score = scoreForAdvantageOrWin();
         }
         else
         {
-            for (int i=1; i<3; i++)
+            score = runningScore(score);
+        }
+        return score;
+    }
+
+    private String runningScore(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = scorePlayer1;
+            else { score +="-"; tempScore = scorePlayer2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = scorePlayer1;
-                else { score+="-"; tempScore = scorePlayer2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
             }
         }
         return score;
+    }
+
+    private String scoreForAdvantageOrWin() {
+        String score;
+        int minusResult = scorePlayer1 - scorePlayer2;
+
+        if (minusResult==1) score ="Advantage " + player1Name;
+        else if (minusResult ==-1) score ="Advantage " + player2Name;
+
+        else if (minusResult>=2) score = "Win for " + player1Name;
+        else score ="Win for " + player2Name;
+        return score;
+    }
+
+    private String scoreWhenPlayersAreEven() {
+        String score;
+        switch (scorePlayer1)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    private boolean isScoreEven() {
+        return scorePlayer1 == scorePlayer2;
     }
 }
